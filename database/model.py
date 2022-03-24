b54fd19c157b83da225453a25fa7408284ff5d4f
tapybara <okuyama0529work@gmail.com>
2022-03-25 00:49:32 +0900

    VARCHAR requires a length on dialect mysqlのエラー修正

--- database/model.py
+++ database/model.py
@@ -7,16 +7,16 @@ from setting import ENGINE
 class History(Base):
     __tablename__ = 'history'
     user_id = Column('user_id', Integer, primary_key = True)    #ユーザーID
-    user = Column('user', String, nullable=False)               #ユーザー名
-    refer = Column('refer', String, nullable=False)             #アクセス元IP
+    user = Column('user', String(60), nullable=False)               #ユーザー名
+    refer = Column('refer', String(20), nullable=False)             #アクセス元IP
     time = Column('time', DateTime, nullable=False)             #アクセス日時
-    param = Column('param', String)                             #送信内容（ON/OFF情報）
+    param = Column('param', String(5))                             #送信内容（ON/OFF情報）
 
 class User(Base):
     __tablename__ = 'users'
     user_id = Column('user_id', Integer, primary_key = True)    #ユーザーID
-    user = Column('user', String, nullable=False)               #ユーザー名
-    password = Column('password', String, nullable=False)       #パスワード
+    user = Column('user', String(60), nullable=False)               #ユーザー名
+    password = Column('password', String(20), nullable=False)       #パスワード
     reg_date = Column('reg_date', DateTime, nullable=False)     #登録日
 
 def main(args):
